fe1316642c177a6257d5ee7ef3a9b41861cd423f
Isaac0099 <isaacchamberlain01@gmail.com>
2025-01-21 00:45:48 -0700

    made crawlPage recursive

--- crawl.js
+++ crawl.js
@@ -1,24 +1,50 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  // if this is an offsite URL, bail immediately
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedURL = normalizeURL(currentURL);
+
+  // if we've already visisted this page just increase the count but don't visit again
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
+    return pages;
+  }
+
+  //initialize this page. Since we made it this far we know t
+  pages[normalizedURL] = 1;
+
   console.log(`actively crawling ${currentURL}`);
+  let htmlBody = "";
 
   try {
     const resp = await fetch(currentURL);
 
     if (resp.status > 399) {
-      console.log(`Error in fetch with status code: ${resp.status}, on page: ${currentURL}`);
-      return;
+      console.log(`HTTP error with status code: ${resp.status}, on page: ${currentURL}`);
+      return pages;
     }
 
     const contentType = resp.headers.get("content-type");
     if (!contentType.includes("text/html")) {
       console.log(`content type is not html. Type: ${contentType}, on page: ${currentURL}`);
-      return;
+      return pages;
     }
+
+    htmlBody = await resp.text();
   } catch (err) {
     console.log(`Error in fetch: ${err.message}, on page ${currentURL}`);
   }
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+  for (const nextURL of nextURLs) {
+    pages = await crawlPage(baseURL, nextURL, pages);
+  }
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
