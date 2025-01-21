a306551909cce9dc2f36f0e7c158620a66ac6f1d
Isaac0099 <isaacchamberlain01@gmail.com>
2025-01-21 00:01:32 -0700

    started building main function plus some error handling for crawlPage

--- crawl.js
+++ crawl.js
@@ -1,5 +1,26 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`actively crawling ${currentURL}`);
+
+  try {
+    const resp = await fetch(currentURL);
+
+    if (resp.status > 399) {
+      console.log(`Error in fetch with status code: ${resp.status}, on page: ${currentURL}`);
+      return;
+    }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`content type is not html. Type: ${contentType}, on page: ${currentURL}`);
+      return;
+    }
+  } catch (err) {
+    console.log(`Error in fetch: ${err.message}, on page ${currentURL}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -37,6 +58,7 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
+  crawlPage,
   normalizeURL,
   getURLsFromHTML,
 };
