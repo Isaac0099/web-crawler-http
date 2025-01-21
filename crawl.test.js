a0191243ed1f37fd84325372c2b88fb67abecadc
Isaac0099 <isaacchamberlain01@gmail.com>
2025-01-20 23:00:54 -0700

    added getURLsFromHTML with tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { getURLsFromHTML, normalizeURL } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL", () => {
@@ -19,5 +19,59 @@ test("normalizeURL capital invariance", () => {
   const input = "https://BLOG.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML", () => {
+  const inputHTMLBody = `
+ <html>
+    <body>
+        <a href="https://blog.boot.dev/">
+            Boot.dev Blog
+        </a>
+        <a href="https://google.com">
+            google link
+        </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/", "https://google.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+   <html>
+      <body>
+          <a href="/path/">
+              Boot.dev Blog
+          </a>
+          <a href="https://google.com">
+              google link
+          </a>
+      </body>
+  </html>
+  `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/", "https://google.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+     <html>
+        <body>
+            <a href="invalid">
+                invalid URL
+            </a>
+        </body>
+    </html>
+    `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
